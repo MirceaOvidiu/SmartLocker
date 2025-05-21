e2382c7494167f5f398c2460b3f6022a5d852520
Dodi Mircea Ovidiu <44977584+MirceaOvidiu@users.noreply.github.com>
2025-05-21 17:12:36 +0300

    changed to FSM logic

--- sketch_may14a.ino
+++ sketch_may14a.ino
@@ -8,7 +8,7 @@
 // RFID pins
 #define SS_PIN A0
 #define RST_PIN A1
-MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 const int ROW_NUM = 4;
 const int COLUMN_NUM = 4;
@@ -23,32 +23,47 @@ char keys[ROW_NUM][COLUMN_NUM] = {
 byte pin_rows[ROW_NUM] = {9, 8, 7, 6};
 byte pin_column[COLUMN_NUM] = {5, 4, 3, 2};
 
-Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
-
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
-
 Servo myservo;
-const int load_cell = A3;
-const int buzzer = A2;  
 
+const int load_cell = A3;
+const int buzzer = A2;
 #define BUTTON_PIN 13
 
 int PASSLEN = 5;
 char data[5] = {0};
 char password[5] = "1111";
+char newPin[5] = {0};
+char confirmPin[5] = {0};
 byte data_count = 0;
+byte newPinCount = 0;
+byte confirmPinCount = 0;
 char key;
 
 byte tries = 3;
-int alert = 0;
-
-// Track lock state
+bool alert = false;
 bool isLocked = true;
 
-// RFID card UID to toggle lock: 33 B9 87 1F
-byte toggleUID[4] = {0x33, 0xB9, 0x87, 0x1F};
+// Master card UID: 62 47 E2 55
+byte masterUID[4] = {0x62, 0x47, 0xE2, 0x55};
+// User card UID: 33 B9 87 1F
+byte userUID[4] = {0x33, 0xB9, 0x87, 0x1F};
 
-void setup(){
+enum State {
+  STATE_IDLE,
+  STATE_STATUS,
+  STATE_ALERT,
+  STATE_MASTER_MENU,
+  STATE_NEW_PIN_FIRST,
+  STATE_NEW_PIN_SECOND
+};
+
+State currentState = STATE_IDLE;
+unsigned long stateStartTime = 0;
+const unsigned long statusDisplayTime = 1500;
+
+void setup() {
   Serial.begin(9600);
   
   lcd.init();
@@ -56,51 +71,50 @@ void setup(){
   lcd.backlight();
   
   myservo.attach(10);
-  lockDoor(); // Start locked
+  lockDoor();
   
   pinMode(buzzer, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   
-  // Initialize RFID
   SPI.begin();
   rfid.PCD_Init();
-  Serial.println("RFID Reader initialized.");
   
-  // Print first-time instructions
   lcd.clear();
   lcd.print("System Ready");
-  delay(2000);
+  delay(1000);
   lcd.clear();
 }
 
-void loop(){
-  // --- Load cell reading and buzzer logic ---
+void loop() {
   int loadCellValue = analogRead(load_cell);
-  Serial.print("Load cell reading: ");
-  Serial.println(loadCellValue);
-
-  // If load cell reading drops under 35, sound alarm
-  if (loadCellValue < 35) {
-    tone(buzzer, 1000); // Sound buzzer at 1kHz
-  } else if (alert == 0) {
-    noTone(buzzer);     // Only stop buzzer if not in alert mode
+  
+  if (loadCellValue < 35 && !alert) {
+    tone(buzzer, 1000);
+  } else if (!alert) {
+    noTone(buzzer);
   }
 
-  // --- RFID logic ---
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-    if (isToggleCard()) {
-      if (alert == 1) {
-        // Stop alert mode and buzzer
-        alert = 0;
+    if (alert) {
+      if (isMasterCard()) {
+        alert = false;
         tries = 3;
         noTone(buzzer);
         lcd.clear();
         lcd.print("RFID Authorized!");
         unlockDoor();
         isLocked = false;
-        delay(1500);
+        stateStartTime = millis();
+        currentState = STATE_STATUS;
+      }
+    } else {
+      if (isMasterCard()) {
         lcd.clear();
-      } else {
+        lcd.print("A: Change PIN");
+        lcd.setCursor(0,1);
+        lcd.print("B: Exit");
+        currentState = STATE_MASTER_MENU;
+      } else if (isUserCard()) {
         lcd.clear();
         if (isLocked) {
           lcd.print("RFID Unlocked!");
@@ -111,127 +125,181 @@ void loop(){
           lockDoor();
           isLocked = true;
         }
-        delay(1500);
+        stateStartTime = millis();
+        currentState = STATE_STATUS;
+      } else {
         lcd.clear();
+        lcd.print("Wrong RFID card!");
+        tone(buzzer, 2000);
+        delay(3000);
+        noTone(buzzer);
+        lcd.clear();
+        currentState = STATE_IDLE;
       }
     }
-    // Halt PICC and stop encryption
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
   }
 
-  // --- Keypad and lock logic ---
-  if(alert == 0) {
-    lcd.setCursor(0,0);
-    if(isLocked) {
-      lcd.print("Enter password   ");
-      lcd.setCursor(0,1);
-      lcd.print("PIN: ");
-    } else {
-      lcd.print("Enter password   ");
-      lcd.setCursor(0,1);
-      lcd.print("PIN: ");
-    }
+  switch (currentState) {
+    case STATE_IDLE:
+      handleIdleState();
+      break;
 
-    // Show the current typed password
-    for (byte i = 0; i < data_count; i++) {
-      lcd.setCursor(5 + i, 1);
-      lcd.print(data[i]);
-    }
-    // Clear the rest of the line if user deletes chars
-    for (byte i = data_count; i < PASSLEN-1; i++) {
-      lcd.setCursor(5 + i, 1);
-      lcd.print(" ");
-    }
-
-    key = keypad.getKey();
-    if(key && key != '#') {
-      if(data_count < PASSLEN-1) { // Prevent buffer overflow
-        data[data_count] = key;
-        data_count++;
+    case STATE_STATUS:
+      if (millis() - stateStartTime > statusDisplayTime) {
+        lcd.clear();
+        currentState = alert ? STATE_ALERT : STATE_IDLE;
       }
-    }
+      break;
 
-    if(key == '#') {
-      lcd.clear();
+    case STATE_ALERT:
+      lcd.setCursor(0,0);
+      lcd.print("Alert!");
+      lcd.setCursor(0,1);
+      lcd.print("Use Master Card");
+      tone(buzzer, 500);
+      break;
 
-      data[data_count] = '\0'; // Null-terminate for strcmp
+    case STATE_MASTER_MENU:
+      key = keypad.getKey();
+      if (key == 'A') {
+        lcd.clear();
+        lcd.print("Enter new PIN:");
+        lcd.setCursor(0,1);
+        currentState = STATE_NEW_PIN_FIRST;
+        newPinCount = 0;
+        memset(newPin, 0, sizeof(newPin));
+      } else if (key == 'B') {
+        lcd.clear();
+        currentState = STATE_IDLE;
+      }
+      break;
 
-      if(!strcmp(data, password)) {
-        if(isLocked) {
-          lcd.print("Unlocked");
-          unlockDoor();
-          isLocked = false;
-        } else {
-          lcd.print("Locked");
-          lockDoor();
-          isLocked = true;
+    case STATE_NEW_PIN_FIRST:
+      key = keypad.getKey();
+      if (key && key != '#') {
+        if (newPinCount < PASSLEN-1) {
+          newPin[newPinCount] = key;
+          lcd.setCursor(newPinCount, 1);
+          lcd.print(key);
+          newPinCount++;
         }
-        delay(1500);
       }
-      else {
-        lcd.print("Incorrect");
-        tries--;
+      if (key == '#') {
+        lcd.clear();
+        lcd.print("Confirm PIN:");
+        lcd.setCursor(0,1);
+        currentState = STATE_NEW_PIN_SECOND;
+        confirmPinCount = 0;
+        memset(confirmPin, 0, sizeof(confirmPin));
+      }
+      break;
+
+    case STATE_NEW_PIN_SECOND:
+      key = keypad.getKey();
+      if (key && key != '#') {
+        if (confirmPinCount < PASSLEN-1) {
+          confirmPin[confirmPinCount] = key;
+          lcd.setCursor(confirmPinCount, 1);
+          lcd.print(key);
+          confirmPinCount++;
+        }
+      }
+      if (key == '#') {
+        newPin[newPinCount] = '\0';
+        confirmPin[confirmPinCount] = '\0';
+        if (strcmp(newPin, confirmPin) == 0) {
+          strcpy(password, newPin);
+          lcd.clear();
+          lcd.print("PIN Changed!");
+        } else {
+          lcd.clear();
+          lcd.print("PINs don't match!");
+        }
         delay(1000);
         lcd.clear();
-        lcd.setCursor(0,0);
-
-        if(tries == 0) {
-          lcd.print("No more tries");
-          alert = 1; // Enter alert mode
-        }
-        else {
-          lcd.print("You can try");
-          lcd.setCursor(0, 1);
-          lcd.print(tries);
-          if(tries == 1) {
-            lcd.print(" more time");
-          }
-          else {  
-            lcd.print(" more times");
-          }
-        }
-        delay(1000);
+        currentState = STATE_IDLE;
       }
-
-      lcd.clear();
-
-      // Clear password buffer
-      while(data_count !=0) {
-        data[--data_count] = 0; 
-      }
-    }
-  }
-  else {
-    // Alert mode - buzzer ON, only RFID can stop
-    lcd.setCursor(0,0);
-    lcd.print("Alert!");
-    lcd.setCursor(0, 1);
-    lcd.print("Use RFID key");
-    tone(buzzer, 500); 
-    // Do not allow manual override or stop buzzer except with RFID
-    delay(200); // Small delay to prevent CPU hogging
+      break;
   }
 }
 
-// Function to unlock door with servo
+void handleIdleState() {
+  lcd.setCursor(0,0);
+  lcd.print("Enter password   ");
+  lcd.setCursor(0,1);
+  lcd.print("PIN: ");
+
+  for (byte i = 0; i < data_count; i++) {
+    lcd.setCursor(5 + i, 1);
+    lcd.print(data[i]);
+  }
+  for (byte i = data_count; i < PASSLEN-1; i++) {
+    lcd.setCursor(5 + i, 1);
+    lcd.print(" ");
+  }
+
+  key = keypad.getKey();
+  if (key && key != '#') {
+    if (data_count < PASSLEN-1) {
+      data[data_count] = key;
+      data_count++;
+    }
+  }
+
+  if (key == '#') {
+    lcd.clear();
+    data[data_count] = '\0';
+
+    if (!strcmp(data, password)) {
+      if (isLocked) {
+        lcd.print("Unlocked");
+        unlockDoor();
+        isLocked = false;
+      } else {
+        lcd.print("Locked");
+        lockDoor();
+        isLocked = true;
+      }
+      stateStartTime = millis();
+      currentState = STATE_STATUS;
+    } else {
+      lcd.print("Incorrect");
+      tries--;
+      if (tries == 0) {
+        alert = true;
+        currentState = STATE_ALERT;
+      }
+      stateStartTime = millis();
+      currentState = STATE_STATUS;
+    }
+    while (data_count != 0) {
+      data[--data_count] = 0;
+    }
+  }
+}
+
 void unlockDoor() {
   myservo.write(130);
 }
 
-// Function to lock door with servo
 void lockDoor() {
   myservo.write(10);
 }
 
-// Function to check if the detected card is the toggle card
-bool isToggleCard() {
+bool isMasterCard() {
   if (rfid.uid.size != 4) return false;
   for (byte i = 0; i < 4; i++) {
-    if (rfid.uid.uidByte[i] != toggleUID[i]) {
-      return false;
-    }
+    if (rfid.uid.uidByte[i] != masterUID[i]) return false;
   }
-  Serial.println("Toggle card detected!");
+  return true;
+}
+
+bool isUserCard() {
+  if (rfid.uid.size != 4) return false;
+  for (byte i = 0; i < 4; i++) {
+    if (rfid.uid.uidByte[i] != userUID[i]) return false;
+  }
   return true;
 }
